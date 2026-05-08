ca8520b36571f57dff13df07fc33c76a635898ee
Ricardo Del Vecchio <ric.vecchio@gmail.com>
2026-05-08 15:32:41 -0300

    feat: add roles field to UserResponse

--- src/main/kotlin/br/pucpr/authserver/users/responses/UserResponse.kt
+++ src/main/kotlin/br/pucpr/authserver/users/responses/UserResponse.kt
@@ -6,6 +6,12 @@ data class UserResponse(
     val id: Long,
     val email: String,
     val name: String,
+    val roles: List<String>
 ) {
-    constructor(user: User) : this(user.id!!, user.email, user.name)
+    constructor(user: User) : this(
+        id = user.id!!,
+        email = user.email,
+        name = user.name,
+        roles = user.roles.map { it.name }.sorted()
+    )
 }
